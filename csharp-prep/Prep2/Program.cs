d9726b03b147637ea0aaf44a82c57cd53954e512
javiera-MLJ <lor24003@byui.edu>
2024-11-14 00:18:29 -0300

    prep activities

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int percentage = int.Parse(userInput);
+
+        string grade = "";
+
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+
+        else if (percentage >= 60)
+        {
+            grade = "D";
+        }
+
+        else if (percentage < 60)
+        {
+            grade = "F";
+        }
+
+        Console.WriteLine($"Your grade is : {grade}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("congratulations, you have passed the course");
+        }
+
+        else
+        {
+            Console.WriteLine("We are very sorry, you have not passed the course, do not give up, try again");
+        }
+
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,49 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        while (true)
+        {
+
+            Random randomGenerator = new Random();
+            int magicN = randomGenerator.Next(1, 101);
+            int guess = -1;
+
+
+            while (guess != magicN)
+            {
+
+                Console.Write("What is your guess? ");
+                guess = int.Parse(Console.ReadLine());
+
+
+                if (guess < magicN)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if (guess > magicN)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+            }
+
+            Console.Write("do you want to play again? ");
+            string response = Console.ReadLine();
+
+            if (response != "yes")
+            {
+                Console.WriteLine("Thanks for playing! Goodbye!");
+                break;
+            }
+        }
     }
 }
